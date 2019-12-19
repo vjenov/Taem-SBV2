bdd459760f1c1517d6911364a04f363e955e0a46
vjenov <jeno0315@naver.com>
2019-12-19 18:12:35 +0900

    first commit

---
+++ src/main/java/com/taem/web/TaemSbv2Application.java
@@ -0,0 +1,13 @@
+package com.taem.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaemSbv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaemSbv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/taem/web/TaemSbv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taem.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaemSbv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
